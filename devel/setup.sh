4cd253c24c44a257648f72f7fbb27dd7dd532ef7
Remo Senekowitsch <remo@buenzli.dev>
2025-05-23 00:00:29 +0200

    update setup script

--- devel/setup.sh
+++ devel/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-sudo dnf install -y \
-    openscad \
-    clang-tools-extra # for formatting with the vscode extension
+sudo dnf install -y openscad
 
-cargo binstall -y openscad-lsp # for helix
+cargo binstall -y openscad-lsp topiary-cli # LSP and formatter
